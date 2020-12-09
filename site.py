becc8f9854b5e7961f6708fa0167b396bbba7ca9
ameyagh <ameyapg2@illinois.edu>
2020-12-08 22:38:32 -0600

    progress

--- site.py
+++ site.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '092487f8e26e1ddc22d41f919418e32a'
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
     # introduction for our project have two buttons here
     return render_template('home.html', title='Home')
@@ -15,11 +15,16 @@ def home():
 def rel1():
     return render_template('rel1.html', title='Hinduism')
 # page for Judaism
-@app.route('/rel2', methods=['GET', 'POST'])
+@app.route('/rel2', methods=['GET'])
 def rel2():
     return render_template('rel2.html', title='Judaism')
+
+# page for definition of religion
+@app.route('/def', methods=['GET'])
+def defn():
+    return render_template('def.html', title='Religion Definition')
 
 
 if __name__ == '__main__':
     app.secret_key = app.config['SECRET_KEY']
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
